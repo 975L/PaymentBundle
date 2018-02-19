be7827f933a64db4269433d4a8059a53135e2a94
Laurent Marquet <laurent.marquet@laposte.net>
2018-02-19 14:37:35 +0100

    v1.4.1 ------ - Modified payment page (19/02/2018)

--- Controller/PaymentController.php
+++ Controller/PaymentController.php
@@ -357,4 +357,117 @@ class PaymentController extends Controller
         //Redirects to payment
         return $this->redirectToRoute('payment_display');
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//REFUND
+    /**
+     * @Route("/payment/refund/{orderId}",
+     *      name="payment_refund",
+     *      requirements={"orderId": "^[0-9\-]+$"})
+     * @Method({"GET", "HEAD"})
+     */
+    public function refundAction(Request $request, $orderId)
+    {
+        //Gets the user
+        $user = $this->getUser();
+
+        if ($user !== null && $this->get('security.authorization_checker')->isGranted($this->getParameter('c975_l_payment.roleNeeded'))) {
+            //Gets the manager
+            $em = $this->getDoctrine()->getManager();
+
+            //Gets repository
+            $repository = $em->getRepository('c975L\PaymentBundle\Entity\Payment');
+
+            //Loads from DB
+            $payment = $repository->findOneByOrderId($orderId);
+
+            //Not existing payment
+            if (!$payment instanceof Payment) {
+                throw $this->createNotFoundException();
+            }
+
+            return $this->render('@c975LPayment/pages/refund.html.twig', array(
+                'payment' => $payment,
+            ));
+        }
+
+        //Access is denied
+        throw $this->createAccessDeniedException();
+    }
+
+//CONFIRM REFUND
+    /**
+     * @Route("/payment/confirm/refund/{orderId}",
+     *      name="payment_confirm_refund",
+     *      requirements={"orderId": "^[0-9\-]+$"})
+     * @Method({"GET", "HEAD"})
+     */
+    public function confirmRefundAction(Request $request, $orderId)
+    {
+        //Gets the user
+        $user = $this->getUser();
+
+        if ($user !== null && $this->get('security.authorization_checker')->isGranted($this->getParameter('c975_l_payment.roleNeeded'))) {
+            //Gets the manager
+            $em = $this->getDoctrine()->getManager();
+
+            //Gets repository
+            $repository = $em->getRepository('c975L\PaymentBundle\Entity\Payment');
+
+            //Loads from DB
+            $payment = $repository->findOneByOrderId($orderId);
+
+            //Not existing payment
+            if (!$payment instanceof Payment) {
+                throw $this->createNotFoundException();
+            }
+
+dump($payment);
+dump($payment->getStripeToken());
+
+            //Creates the refund on Stripe's servers - This will refund the user's card
+            try {
+                //Gets paymentService
+                $paymentService = $this->get(\c975L\PaymentBundle\Service\PaymentService::class);
+
+                //Do the Stripe refund
+                \Stripe\Stripe::setApiKey($paymentService->getSecretKey($this->getParameter('c975_l_payment.live')));
+                \Stripe\Refund::create(array(
+                    'charge' => $payment->getStripeToken(),
+                ));
+            } catch (Exception $e) {
+                //Something else happened, completely unrelated to Stripe
+                $errMessage = $e->getJsonBody()['error']['message'];
+                $errCode = 'ErrStripeRefund';
+                $displayError = false;
+            }
+
+
+
+
+            return $this->render('@c975LPayment/pages/refund.html.twig', array(
+                'payment' => $payment,
+            ));
+        }
+
+        //Access is denied
+        throw $this->createAccessDeniedException();
+    }
 }
